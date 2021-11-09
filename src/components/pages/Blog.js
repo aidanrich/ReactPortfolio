a6a87d6060f1c706e9f0885b21add99f1fbbe4e3
aorich <rich.aidan@gmail.com>
2021-11-09 08:22:16 -0500

    portfolio page is more responsive

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/Card.css';
-import fitness from '../assets/fitnesspic.JPG';
-import newTown from '../assets/new2town.JPG';
+import fitness from '../assets/fitnesspic2.jpg';
+import newTown from '../assets/new2town2.jpg';
 
 export default function Blog() {
   return (
@@ -11,8 +11,18 @@ export default function Blog() {
       <h1 className="heading">Fitness Tracker</h1>
       <p className="content">
         <img src={fitness} alt="logo" />
-
       </p>
+      <p className="content">
+        The fitness tracker is a fullstack app that helps a user track and aggregate their workout routines over time. For this piece I worked primarily on the backend database and routes linking to the front end JS.
+          </p>
+          <ul className="nav">
+                    <li>
+                    <a href="https://github.com/aidanrich/fitness-tracker" className="heading" target="_blank" rel="noreferrer noopener">Git Repo</a>
+                    </li>
+                    <li>
+                    <a href="https://vast-everglades-43611.herokuapp.com/" className="heading" target="_blank" rel="noreferrer noopener">Live Site</a>
+                    </li>
+                </ul>
     </div>
     <div className="box-main">
         <h1 className="heading">New2Town</h1>
@@ -20,6 +30,17 @@ export default function Blog() {
           <img src={newTown} alt="logo" />
 
         </p>
+        <p className="content">
+        New2Town is a fullstack social media app using MySQL, Express.js and Handlebars. In this group project I worked on creating the models for the various data sets we needed and worked on the Profile page, Friendslist and helped with the login and new user signup functionality. Team work and communication were key to success on this project.
+        </p>
+        <ul className="nav">
+                    <li>
+                    <a href="https://github.com/aidanrich/new2town" className="heading" target="_blank" rel="noreferrer noopener">Git Repo</a>
+                    </li>
+                    <li>
+                    <a href="https://protected-anchorage-26140.herokuapp.com/" className="heading" target="_blank" rel="noreferrer noopener">Live Site</a>
+                    </li>
+                </ul>
       </div>
       </div>
       </>
