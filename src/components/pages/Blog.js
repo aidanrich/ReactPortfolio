ab4a69bb9fa14a074016e9e816ec88b0075ad7ce
aorich <rich.aidan@gmail.com>
2021-11-09 19:04:37 -0500

    oulined links to github and live site on project page

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -16,14 +16,16 @@ export default function Blog() {
       <p className="content">
         The fitness tracker is a fullstack app that helps a user track and aggregate their workout routines over time. For this piece I worked primarily on the backend database and routes linking to the front end JS.
           </p>
+          <p className='above'>
           <ul className="nav">
                     <li>
-                    <a href="https://github.com/aidanrich/fitness-tracker" className="heading" target="_blank" rel="noreferrer noopener">Git Repo</a>
+                    <a href="https://github.com/aidanrich/fitness-tracker" className="heading repo" target="_blank" rel="noreferrer noopener">Git Repo</a>
                     </li>
                     <li>
-                    <a href="https://vast-everglades-43611.herokuapp.com/" className="heading" target="_blank" rel="noreferrer noopener">Live Site</a>
+                    <a href="https://vast-everglades-43611.herokuapp.com/" className="heading repo" target="_blank" rel="noreferrer noopener">Live Site</a>
                     </li>
                 </ul>
+                </p>
     </div>
     <div className="box-main">
         <h1 className="heading">New2Town</h1>
@@ -34,14 +36,16 @@ export default function Blog() {
         <p className="content">
         New2Town is a fullstack social media app using MySQL, Express.js and Handlebars. In this group project I worked on creating the models for the various data sets we needed and worked on the Profile page, Friendslist and helped with the login and new user signup functionality. Team work and communication were key to success on this project.
         </p>
+        <p className='above'>
         <ul className="nav">
                     <li>
-                    <a href="https://github.com/aidanrich/new2town" className="heading" target="_blank" rel="noreferrer noopener">Git Repo</a>
+                    <a href="https://github.com/aidanrich/new2town" className="heading repo" target="_blank" rel="noreferrer noopener">Git Repo</a>
                     </li>
                     <li>
-                    <a href="https://protected-anchorage-26140.herokuapp.com/" className="heading" target="_blank" rel="noreferrer noopener">Live Site</a>
+                    <a href="https://protected-anchorage-26140.herokuapp.com/" className="heading repo" target="_blank" rel="noreferrer noopener">Live Site</a>
                     </li>
                 </ul>
+                </p>
       </div>
       <div className="box-main">
         <h1 className="heading">Budget Tracker</h1>
@@ -52,14 +56,16 @@ export default function Blog() {
         <p className="content">
           This app specializes in tracking your expenditures even when offline. Using IndexDB in tandem with a service-worker users can expect data persistence even when on the go in poor connection areas.
         </p>
+        <p className='above'>
         <ul className="nav">
                     <li>
-                    <a href="https://github.com/aidanrich/offline-budget-tracker" className="heading" target="_blank" rel="noreferrer noopener">Git Repo</a>
+                    <a href="https://github.com/aidanrich/offline-budget-tracker" className="heading repo" target="_blank" rel="noreferrer noopener">Git Repo</a>
                     </li>
                     <li>
-                    <a href="https://aqueous-citadel-52972.herokuapp.com" className="heading" target="_blank" rel="noreferrer noopener">Live Site</a>
+                    <a href="https://aqueous-citadel-52972.herokuapp.com" className="heading repo" target="_blank" rel="noreferrer noopener">Live Site</a>
                     </li>
                 </ul>
+                </p>
       </div>
       </div>
       </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../styles/Card.css';
 
-
 export default function Home() {
 
   return (
