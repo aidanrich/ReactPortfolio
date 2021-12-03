ee20653e389b6e75bd37182e9b3770b4c9d2a60e
aorich <rich.aidan@gmail.com>
2021-12-03 11:44:41 -0500

    hashrouter added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 // import Navbar from './components/Navbar';
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
+import About from './components/pages/About';
 // import Card from './components/Card';
 import './App.css';
 
 
 function App() {
   return (
-    <><main>
+    <>
+    <HashRouter>
+    <main>
       <div>
         <Header />
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path ="/projects" element={<Projects />} />
+        <Route path ="/contact" element={<Contact />} />
+        <Route path ="/about" element={<About />} />
+        
+        </Routes>
 
       </div>
     </main>
       <div>
         <Footer />
       </div>
+      </HashRouter>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,40 +4,26 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import { Link } from "react-router-dom"
 import './styles/Header.css';
 
 import iconOfMe from './assets/iconOfMe2.gif'
 
 export default function Header() {
 
-  const [currentPage, setCurrentPage] = useState('Home');
-
-
-  const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
-    if (currentPage === 'About') {
-      return <About />;
-    }
-    if (currentPage === 'Projects') {
-      return <Projects />;
-    }
-    return <Contact />;
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div>
-      
-      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-     
+      <ul className="nav">
+      <Link className="nav-item heading" to="/">Home</Link>
+      <Link className="nav-item heading" to="/about">About</Link>
+      <Link className="nav-item heading" to="/projects">Projects</Link>
+      <Link className="nav-item heading" to="/contact">Contact</Link>
+     </ul>
       <header className="header">
       <h1><img className="sketch" src={iconOfMe} alt="My Pic" />Aidan's Portfolio</h1>
     </header>
-    <div>
-      {renderPage()}</div>
+    
     </div>
   );
 
